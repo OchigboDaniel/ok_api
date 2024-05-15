648a366565ded4dcb366aa97802fff20ca1ae1e4
unknown <ochigbodaniel240@gmail.com>
2024-05-15 12:57:18 -0700

    deleted the app.js file

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-const app = require("./app");
+const express = require('express');
+const body_parser = require('body-parser');
+const adminRoute = require('./routes/admin.routes');
+
+const app = express();
 
 const port = 2000;
+
+app.use(body_parser.json())
+
+app.use('/', adminRoute);
+
+app.use(express.urlencoded);
+
+app.use(express.json())
 
 app.listen(port, ()=>{
     console.log(`App is running on port http://localhost:${port}`);
